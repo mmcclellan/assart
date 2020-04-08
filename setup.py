2c925d226ce19f01ad5fbe1b87f42f43473a9aeb
Michael McClellan <mcclellan.m@gmail.com>
2020-04-08 12:43:44 -0400

    Add setup.py configuration

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="assart",
+    version="0.1.0",
+    url="https://github.com/mmcclellan/assart",
+    license='MIT',
+    author="Michael McClellan",
+    author_email="mcclellan.m@gmail.com",
+    description="A simple s3 analytics reporting tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
