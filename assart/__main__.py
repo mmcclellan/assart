28c4ad8c2d9fc0ae9a6b5e9be957725fc1d9d8fd
Michael McClellan <mcclellan.m@gmail.com>
2020-04-08 12:13:47 -0400

    Add argparse

--- assart/__main__.py
+++ assart/__main__.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+import argparse
 import os
 
 
 def main():
+    parser = argparse.ArgumentParser(description='A simple S3 analytics reporting tool')
+    parser.add_argument(
+        '-u', '--unit', required=False, type=str, choices=['kB', 'MB', 'GB'], help='Unit of Size')
+    args = parser.parse_args()
     print("Hello World!")
+    if args.unit:
+        print("Units:", args.unit)
 
 
 if __name__ == "__main__":
